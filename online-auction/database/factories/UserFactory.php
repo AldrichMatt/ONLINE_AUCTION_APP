6cab97e10c0a15785f6fe755d3e51e082134d2ca
AldrichMatt <mailbox.aldrich@gmail.com>
2023-01-27 14:48:32 +0800

    seeding and configuration

--- online-auction/database/factories/UserFactory.php
+++ online-auction/database/factories/UserFactory.php
@@ -18,11 +18,10 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
+            'full_name' => fake()->name(),
+            'username' => fake()->unique()->name(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
+            'telephone' => fake()->phoneNumber(),
         ];
     }
 

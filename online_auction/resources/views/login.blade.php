3b8a427846bddf34c3718094c9dbee8304f2d47c
AldrichMatt <mailbox.aldrich@gmail.com>
2023-02-02 10:12:27 +0800

    carousel view

--- online_auction/resources/views/login.blade.php
+++ online_auction/resources/views/login.blade.php
@@ -3,6 +3,9 @@
     <x-navbar>
         
     </x-navbar>
+    <x-carousel>
+        
+    </x-carousel>
     <div class="container">
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt, eius.
     </div>

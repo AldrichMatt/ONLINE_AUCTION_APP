8eb07a4a06e100bed10ec47fe678b9de724cf85a
AldrichMatt <mailbox.aldrich@gmail.com>
2023-02-17 16:52:12 +0800

    add new table

--- online_auction/app/Models/RunningOffer.php
+++ online_auction/app/Models/RunningOffer.php
@@ -5,17 +5,18 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class AuctionHistory extends Model
+class RunningOffer extends Model
 {
     const UPDATED_AT = null;
     const CREATED_AT = null;
     use HasFactory;
+
     protected $fillable = [
-        'history_id', //history is auction id + item id + ddmmyyy
+        'offer_id',
         'auction_id',
         'item_id',
         'user_id',
-        'report_date',
-        'sold_price'
+        'offer_date',
+        'offer_price'
     ];
 }
